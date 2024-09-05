874b9d3c567ca32f99f3ff127c71c5840805d42b
feyhide <fahadsheikhessential@gmail.com>
2024-09-06 01:44:41 +0500

    user schema done

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
-const fastify = require("fastify")
+import "dotenv/config"
+import Fastify from "fastify"
+import { connectDB } from "./src/config/connect.js"
 
 const start = async() => {
-    const app = fastify()
+    await connectDB(process.env.MONGO_URI)
+    const app = Fastify()
     const PORT = process.env.PORT||3000
     app.listen({port:PORT,host: "0.0.0.0"},
         (err,addr) => {
